6f4abd0de1ea69315b5773f0c11b25840d03eed3
virat-18-git <ksprddy6@gmail.com>
2024-12-20 19:08:17 +0530

    newfile

---
+++ index.js
@@ -0,0 +1,105 @@
+
+// let a=123;
+// var a="shiva"
+// console.log(a) show error because a arleady used in let 
+
+// numbers:
+// let a=500
+// console.log(a)
+
+// let a=500
+//console.log(x) undefined
+
+// let a="500"
+// console.log(a)
+
+
+// let a="500"
+//  console.log(typeof a)
+
+// let a=500
+//  console.log(typeof a)
+
+
+// let a=500
+//  console.log(typeof 100)
+
+
+// string:
+
+
+// let a="shiva"
+// console.log( typeof a)
+
+// let a="shiva"
+// console.log( typeof typeof a)
+
+// boolean;
+
+// let a=true;
+// console.log(typeof a)
+
+// let a=True;
+// console.log(typeof a)
+
+// undefined;
+
+// let a;
+// console.log(a)
+
+// let a;
+// console.log(typeof a)
+
+// // let a=10;
+// b=a;
+// console.log( typeof b)
+
+// let a=null;
+// console.log(a)
+
+// let a=null;
+// console.log(typeof a)
+
+// let a=null;
+// console.log(typeof typeof a)
+
+// ARRAYS
+// let a=[1,5,"shiva",18,"virat",22,"pawan kalyan"]
+// console.log(a[a.length-3])
+// console.log(a[a.length-1])
+// console.log(a[2])
+// console.log(a[a.lenght])
+// console.log(a.length)
+// console.log(a)
+// console.log(typeof a )
+
+
+// OBJECT:it stores the name with value
+// it is collection of properties and key value pairs
+// {property1,property2,property3}
+// let obj={
+//     key1:value1,
+//     key2:value2,
+//     key3:value3,
+//     key4:value4,
+
+// }
+
+let student={
+    id:"1",
+    name:"shiva",
+    batch:"backbencher",
+    quallifiction:"b.tach",
+    gender:"male",
+    job:"full stack developer",
+    package:"6lpa",
+    city:"HYD",
+    skills:["html","css","javascript","python"]
+}
+// console.log(student)
+// console.log(typeof student)
+// console.log(student.length)
+// console.log(student.job)
+// console.log(student.skills)
+// console.log(student.skills.length)
+// console.log(student.skills[3])
